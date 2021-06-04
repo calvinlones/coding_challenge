7d7ff8b800e664cbee38b449021ccfb422f32a76
Calvin Lones <calvin.j.lones@gmail.com>
2021-06-04 11:55:16 -0500

    added API route for initial github repo data fetch

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
 import logging
+import requests
+import os
 
 import flask
 from flask import Response
@@ -7,11 +9,31 @@ app = flask.Flask("user_profiles_api")
 logger = flask.logging.create_logger(app)
 logger.setLevel(logging.INFO)
 
-
-@app.route("/health-check", methods=["GET"])
-def health_check():
+@app.route("/getStats/<name>", methods=["GET"])
+def getStats(name):
     """
     Endpoint to health check API
     """
-    app.logger.info("Health Check!")
-    return Response("All Good!", status=200)
+    url = f"https://api.github.com/orgs/{name}/repos"
+    userData = requests.get(url).json()
+    totalRepos = len(userData)
+    forkedRepos = 0
+    totalWatchers = 0
+    totalLanguages = []
+    totalTopics = []
+    for repo in userData:
+        if repo['fork'] == True:
+            forkedRepos += 1
+        totalWatchers += repo['watchers_count']
+        langURL = f"https://api.github.com/repos/{name}/{repo['name']}/languages"
+        langData = requests.get(langURL).json()
+        totalLanguages.append(langData)
+        topicURL = f"https://api.github.com/repos/{name}/{repo['name']}/topics"
+        topicData = requests.get(topicURL).json()
+        totalTopics.append(topicData['names'])
+    originalRepos = totalRepos - forkedRepos
+    return Response("Number of original repos: " + str(originalRepos) + "\n" +
+    "Number of forked repos: " + str(forkedRepos) + "\n" +
+    "Total number of watchers: " + str(totalWatchers) + "\n" +
+    "Total languages: " + str(totalLanguages) + "\n" +
+    "Total topics: " + str(totalTopics) + "\n")
